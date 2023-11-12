d601471c989d5d6dcde015aa4549485af0a1f039
vrs000 <vrs.my5@gmail.com>
2023-11-12 13:20:15 +0300

    Update broadcast-server.py

--- broadcast-server.py
+++ broadcast-server.py
@@ -1,12 +1,15 @@
 from datetime import datetime
 import socket
 
+counter = 0
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 sock.bind(("0.0.0.0", 7720))
 while True:    
     data, addr = sock.recvfrom(1024)
+    counter+=1
     cur_time = datetime.now().time()
-    print(f"[{cur_time}]: received {len(data)} bytes from <{addr[0]}:{addr[1]}>")
+    print(f"[{cur_time}]: received {len(data)} bytes from <{addr[0]}:{addr[1]}> | #{counter} ")
     
