7885f6b16e9b1b2bea63966ca82ffc8741bf7ffc
vrs000 <vrs.my5@gmail.com>
2023-11-12 13:15:04 +0300

    Update broadcast-server.py

--- broadcast-server.py
+++ broadcast-server.py
@@ -8,5 +8,5 @@ sock.bind(("0.0.0.0", 7720))
 while True:    
     data, addr = sock.recvfrom(1024)
     cur_time = datetime.now().time()
-    print(f"{cur_time}: received {len(data)} bytes from {addr[0]}:{addr[1]}")
+    print(f"[{cur_time}]: received {len(data)} bytes from <{addr[0]}:{addr[1]}>")
     
