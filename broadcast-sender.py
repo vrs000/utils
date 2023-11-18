371f4514d713c2902e3819455dde323a2321e778
vrs000 <vrs.my5@gmail.com>
2023-11-18 11:14:36 +0300

    Update broadcast-sender.py

--- broadcast-sender.py
+++ broadcast-sender.py
@@ -1,22 +1,23 @@
 import socket
 from time import sleep
-
-
-# sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)  # UDP
-# sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-# sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
-
-# while True:    
-#     sock.sendto(b'hello world', ("255.255.255.255", 7720))
-#     sleep(1)
+from sys import argv
+from datetime import datetime
 
 interfaces = socket.getaddrinfo(host=socket.gethostname(), port=None, family=socket.AF_INET)
 
-allips = [ip[-1][0] for ip in interfaces]
-msg = b'hello world'
+allips = [ip[-1][0] for ip in interfaces if ip[-1][0] != '127.0.0.1']
+msg = b'TEST'
+
+if len(argv)> 1:
+    allips += argv[1:]
+
+counter = 0
+
 while True:
     for ip in allips:
-        print(f'sending on {ip}')
+        cur_time = datetime.now().time()
+        counter+=1
+        print(f'[{cur_time}] Sending from {ip} | #{counter}')
         sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)  # UDP
         sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
         sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
